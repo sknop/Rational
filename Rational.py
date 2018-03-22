2dce91f16d13b132d40d6c9bd231d1d8c7fa7f98
Sven Erik Knop <sknop@perforce.com>
2018-03-22 21:48:47 +0100

    r-type operators don't need Rational check

--- Rational.py
+++ Rational.py
@@ -36,9 +36,7 @@ class Rational:
             raise Exception("Cannot add object of type {} to a Rational object".format(other.__class__))
 
     def __radd__(self, other):
-        if isinstance(other, Rational):
-            return Rational(self.nomin * other.denom + other.nomin * self.denom, self.denom * other.denom)
-        elif isinstance(other, int):
+        if isinstance(other, int):
             return Rational(self.nomin + other * self.denom, self.denom)
         else:
             raise Exception("Cannot add object of type {} to a Rational object".format(other.__class__))
@@ -52,9 +50,7 @@ class Rational:
             raise Exception("Cannot subtract object of type {} to a Rational object".format(other.__class__))
 
     def __rsub__(self, other):
-        if isinstance(other, Rational):
-            return Rational(self.nomin * other.denom - other.nomin * self.denom, self.denom * other.denom)
-        elif isinstance(other, int):
+        if isinstance(other, int):
             return Rational(other * self.denom - self.nomin, self.denom)
         else:
             raise Exception("Cannot subtract object of type {} to a Rational object".format(other.__class__))
@@ -71,9 +67,7 @@ class Rational:
             raise Exception("Cannot multiply object of type {} to a Rational object".format(other.__class__))
 
     def __rmul__(self, other):
-        if isinstance(other, Rational):
-            return Rational(self.nomin * other.nomin, self.denom * other.denom)
-        elif isinstance(other, int):
+        if isinstance(other, int):
             return Rational(self.nomin * other, self.denom)
         else:
             raise Exception("Cannot multiply object of type {} to a Rational object".format(other.__class__))
@@ -87,9 +81,7 @@ class Rational:
             raise Exception("Cannot divide a Rational object with an object of type {}".format(other.__class__))
 
     def __rtruediv__(self, other):
-        if isinstance(other, Rational):
-            return Rational(self.nomin * other.denom, self.denom * other.nomin)
-        elif isinstance(other, int):
+        if isinstance(other, int):
             return Rational(self.denom * other, self.nomin)
         else:
             raise Exception("Cannot divide a Rational object with an object of type {}".format(other.__class__))
